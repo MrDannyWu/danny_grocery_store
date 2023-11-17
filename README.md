281db65a5889af60ede3b292a184cd7e05f6ee67
developer-03 <developer-03@kredito.id>
2023-11-17 10:36:39 +0800

    1. update README.md 2. add bath_ssh_no_pass_login.sh

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # danny_grocery_store
 Danny 杂货铺
+
+Hadoop
+Hive
+Flink SQL
+Flink CDC
+Iceberg
+Trino
+Presto
+Airflow
+Grafana
+Superset
+Arctic
+DolphinScheduler
+Prometheus
+Dinky
---
+++ ssh_script/bath_ssh_no_pass_login.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# bath_ssh_no_pass_login.sh
+# generate ip_list.txt for bath_ssh_no_pass_login.sh
+# cat ip_list.txt
+# 192.168.1.100
+# 192.168.1.101
+# 192.168.1.102
+
+yum install expect -y
+IPS=./ip_list.txt
+PASSWD=$1
+
+if [ ! -n "$PASSWD" ]; then
+  PASSWD=123456
+fi
+
+# ssh-keygen
+for IP in $(cat $IPS)
+  do
+/usr/bin/expect << EOF
+    spawn ssh $IP "rm -rf  /root/.ssh; echo 'StrictHostKeyChecking no' >> /etc/ssh/ssh_config; ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa -q"
+    expect {
+        "yes/no" { send "yes\n";exp_continue }
+        "password" { send "$PASSWD\n" }
+    }
+    expect "*password*" {send "$PASSWD\n";}
+    expect eof
+EOF
+  done
+
+# copy id_rsa.pub
+for IP in $(cat $IPS)
+  do
+/usr/bin/expect << EOF
+    spawn scp root@$IP:/root/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub.$IP
+    expect {
+        "yes/no" { send "yes\n";exp_continue }
+        "password" { send "$PASSWD\n" }
+    }
+    expect "*password*" {send "$PASSWD\n"}
+    expect eof
+EOF
+    cat /root/.ssh/id_rsa.pub.$IP >> /root/.ssh/authorized_keys
+    rm -f /root/.ssh/id_rsa.pub.$IP
+  done
+
+## push authorized_keys
+for COREIP in $(cat $IPS | sed -n '2,$p')
+  do
+/usr/bin/expect << EOF
+    spawn scp /root/.ssh/authorized_keys root@$COREIP:/root/.ssh/authorized_keys
+    expect {
+        "yes/no" { send "yes\n";exp_continue }
+        "password" { send "$PASSWD\n" }
+    }
+    expect "*password*" {send "$PASSWD\n"}
+    expect eof
+EOF
+  done
