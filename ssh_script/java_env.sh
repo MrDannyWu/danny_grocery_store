6d90ecf40eae9ad76af50dfbbc2594b814171422
developer-03 <developer-03@kredito.id>
2023-12-18 16:17:33 +0800

    1. add java_env.sh 2. add python3_bin_env.sh

---
+++ ssh_script/java_env.sh
@@ -0,0 +1,4 @@
+# Configure java environment variables
+export JAVA_HOME=/usr/java/jdk
+export CLASSPATH=.:$CLASSPATH:$JAVA_HOME/lib
+export PATH=$JAVA_HOME/bin:$PATH
---
+++ ssh_script/python3_bin_env.sh
@@ -0,0 +1,2 @@
+# Configure Python3 bin environment variables
+export PATH=/usr/local/python3/bin:$PATH
